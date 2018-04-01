c7d706059c8a2e27dc3c57633cf0115781637227
tolkiana <tolkiana@gmail.com>
2018-03-31 20:59:17 -0400

    Implements reading from database

--- Profiles/ViewController.swift
+++ Profiles/ViewController.swift
@@ -35,16 +35,31 @@ class ViewController: UIViewController {
             return
         }
         let key = profilesRef.childByAutoId().key
-        let profile = [
+        let profile: [String: Any] = [
             "id": key,
             "name": "Neli",
-            "age": "2"
+            "age": "2",
+            "words": [[
+                    "word": "dad",
+                    "pronunciation": "dad"
+                ],
+                [
+                    "word": "mom",
+                    "pronunciation": "mom"
+                ]
+            ]
         ]
         profilesRef.child(key).setValue(profile)
     }
     
     func read() {
+        guard let profilesRef = dataBaseReference?.child("profiles") else {
+            return
+        }
         
+        profilesRef.observe(FIRDataEventType.value) { snapshot in
+            print(snapshot)
+        }
         
     }
 }
